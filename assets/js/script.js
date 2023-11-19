d7a825cf400e1ff8ce003493d9e118eaf518d2e6
SteveR <steve17rouse@gmail.com>
2023-11-18 23:54:30 -0700

    added starter from local repo

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+function searchWeather() {
+    // myAPIKey: 9be6849b96b10ecac5547c81f0c81d08
+    //Add your OpenWeatherMap API call here
+    // Update the 'weatherData' variable with the fetched data
+    // Example: const weatherData = fetchWeatherData(city);
+
+    // After fetching data, you can use it to populate the weather cards
+    // Example: populateWeatherCards(weatherData);
+}
+
+const apiKey = '9be6849b96b10ecac5547c81f0c81d08';
+
+function searchWeather() {
+    const city = 'YOUR_CITY'
+    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+}
+
+function populateWeatherCards(data) {
+    const weatherCardsContainer = document.getElementById('weather-cards-container');
+    weatherCardsContainer.innerHTML = '';
+
+    data.list.forEach(day => {
+        const card = document.createElement('div');
+        card.classList.add('weather-card');
+
+        const dayOfWeek = new Date(day.dt * 1000).toLocaleDateString('en-US', { weekday: 'long' });
+        const iconUrl = `http://openweathermap.org/img/w/${day.weather[0].icon}.png`;
+
+        card.innerHTML = `
+            <p>${dayOfWeek}</p>
+            <img class="weather-image" src="${iconUrl}" alt="${day.weather[0].description}">
+            <p>High: ${day.main.temp_max}&deg;C</p>
+            <p>Low: ${day.main.temp_min}&deg;C</p>
+            <p>Precipitation: ${day.pop}%</p>
+        `;
+
+        weatherCardsContainer.appendChild(card);
+    });
+}
