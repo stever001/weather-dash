480c0cb83d14adcaf963a2e04c8d8128301c2350
SteveR <steve17rouse@gmail.com>
2023-11-19 23:06:47 -0700

    updated JS to create five cards for the forecast days

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,42 @@
-function searchWeather() {
-    // myAPIKey: 9be6849b96b10ecac5547c81f0c81d08
-    //Add your OpenWeatherMap API call here
-    // Update the 'weatherData' variable with the fetched data
-    // Example: const weatherData = fetchWeatherData(city);
-
-    // After fetching data, you can use it to populate the weather cards
-    // Example: populateWeatherCards(weatherData);
-}
-
 const apiKey = '9be6849b96b10ecac5547c81f0c81d08';
 
+document.addEventListener('DOMContentLoaded', () => {
+    const searchHistory = getSearchHistory();
+    displaySearchHistory(searchHistory);
+});
+
+function filterDataForNoon(dataList) {
+    const filteredData = {};
+
+    dataList.forEach(day => {
+        const date = new Date(day.dt * 1000);
+        const dayKey = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+
+        if (!filteredData[dayKey]) {
+            filteredData[dayKey] = {
+                date: date,
+                icon: day.weather[0].icon,
+                temp_max: day.main.temp_max,
+                wind_speed: day.wind.speed,
+                humidity: day.main.humidity,
+                pop: day.pop,
+            };
+        }
+    });
+
+    return Object.values(filteredData);
+}
+
 function searchWeather() {
-    // Get the value of the input field with id 'search-bar'
     const cityInput = document.getElementById('search-bar');
     const city = cityInput.value;
 
-    // Check if the user has entered a city before making the API request
     if (!city) {
         alert('Please enter a city!');
         return;
     }
 
-    // Make the API request
-    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
 
     fetch(apiUrl)
         .then(response => {
@@ -33,7 +47,17 @@ function searchWeather() {
         })
         .then(data => {
             console.log('Weather data:', data);
-            populateWeatherCards(data); // Call the function to populate weather cards
+
+            if (!data.list || data.list.length === 0) {
+                console.error('Empty or invalid data received.');
+                return;
+            }
+
+            const filteredData = filterDataForNoon(data.list);
+
+            populateWeatherCards(filteredData);
+            saveSearchToHistory(city);
+            displaySearchHistory(getSearchHistory());
         })
         .catch(error => {
             console.error('Error fetching weather data:', error);
@@ -44,22 +68,58 @@ function populateWeatherCards(data) {
     const weatherCardsContainer = document.getElementById('weather-cards-container');
     weatherCardsContainer.innerHTML = '';
 
-    data.list.forEach(day => {
+    console.log('Filtered weather data:', data);
+
+    if (!data || data.length === 0) {
+        console.error('Invalid or empty data received.');
+        return;
+    }
+
+    data.forEach(day => {
         const card = document.createElement('div');
         card.classList.add('weather-card');
 
-        const dayOfWeek = new Date(day.dt * 1000).toLocaleDateString('en-US', { weekday: 'long' });
-        const iconUrl = `http://openweathermap.org/img/w/${day.weather[0].icon}.png`;
+        const date = day.date.toLocaleDateString('en-US', { weekday: 'long' });
+        const iconUrl = `http://openweathermap.org/img/w/${day.icon}.png`;
 
         card.innerHTML = `
-            <p>${dayOfWeek}</p>
-            <img class="weather-image" src="${iconUrl}" alt="${day.weather[0].description}">
-            <p>High: ${day.main.temp_max}&deg;C</p>
-            <p>Low: ${day.main.temp_min}&deg;C</p>
-            <p>Precipitation: ${day.pop}%</p>
+            <p>${date}</p>
+            <img class="weather-image" src="${iconUrl}" alt="${day.icon}">
+            <p>High: ${day.temp_max}&deg;F</p>
+            <p>Wind Speed: ${day.wind_speed} mph</p>
+            <p>Humidity: ${day.humidity}%</p>
+            <p>Chance of Precip: ${day.pop}%</p>
         `;
 
         weatherCardsContainer.appendChild(card);
     });
 }
 
+function saveSearchToHistory(city) {
+    const searchHistory = getSearchHistory();
+    if (!searchHistory.includes(city)) {
+        searchHistory.push(city);
+        localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+    }
+}
+
+function getSearchHistory() {
+    return JSON.parse(localStorage.getItem('searchHistory')) || [];
+}
+
+function displaySearchHistory(history) {
+    const searchHistoryContainer = document.getElementById('search-history-container');
+    searchHistoryContainer.innerHTML = '';
+
+    history.forEach(city => {
+        const searchItem = document.createElement('div');
+        searchItem.classList.add('search-history-item');
+        searchItem.textContent = city;
+        searchItem.addEventListener('click', () => {
+            document.getElementById('search-bar').value = city;
+            searchWeather();
+        });
+
+        searchHistoryContainer.appendChild(searchItem);
+    });
+}
